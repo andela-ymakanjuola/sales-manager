4775ac4c233371027498bce342c496e9b1b2b680
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-07-13 11:47:22 +0100

    code clean up

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var express = require('express'),
     app = express();
 
-
 // Public files
 app.use(express.static('public'));
-app.use(express.static('app'));
 
 app.get('/', function(req, res){
   res.sendFile('./public/index.html');
 });
 
-var server = app.listen(process.env.PORT || 5555, function() {
-    console.log('Listening on port %d', server.address().port);
-  });
+var server = app.listen(process.env.PORT || 3000, function() {
+      console.log('Listening on port %d', server.address().port);
+    });
